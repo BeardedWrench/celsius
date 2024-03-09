9e2906be138199ccb76da78e9b7e72281be13914
Terry Brown <tabrownj@gmail.com>
2024-03-09 16:39:09 -0500

    remove unneeded configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  optimizeDeps: {
-    esbuildOptions: {
-      tsconfigRaw: {
-        compilerOptions: {
-          experimentalDecorators: true,
-        },
-      },
-    },
-  },
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ['@babel/plugin-proposal-decorators', { legacy: true }],
-          ['@babel/plugin-proposal-class-properties', { loose: true }],
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
